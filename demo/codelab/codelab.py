e8f14e6d53a6ef210c964ca9cfc564fe58895fd0
Wilson Wang <wilson.wang@bytedance.com>
2025-02-01 20:38:31 +0800

    update comment

--- demo/codelab/codelab.py
+++ demo/codelab/codelab.py
@@ -7,10 +7,22 @@ from spear.utils.tool import register_internal_tool
 
 from spear.proto.tool import BuiltinToolID
 
+# How to run this file:
+# compile the code and execute it with spearlet binary.
+# make sure you also have installed the compiled spear python package.
+# command to install the spear python package:
+#   make build; pip install sdk/python/dist/spear-0.0.1-py3-none-any.whl --force-reinstall
+# command to execute this demo:
+#   spearlet -L <spear_root>/demo/codelab/ exec -t process -f codelab.py
+
+
 agent = client.HostAgent()
 
 
 def test_builtin_tool():
+    """
+    test builtin tool
+    """
     resp = chat.chat(agent,
                      "help me to open sjsu's homepage",
                      model="gpt-4o", builtin_tools=[
@@ -25,8 +37,8 @@ def test_tool_cb(content):
 
     @param content: html content string
     """
-    import webbrowser
     import tempfile
+    import webbrowser
     with tempfile.NamedTemporaryFile(mode='w', suffix='.html', delete=False) as temp_file:
         # Write the HTML content to the temporary file
         temp_file.write(content)
